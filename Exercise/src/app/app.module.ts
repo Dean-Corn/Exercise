b690f43878b1d67d0818fd3271929e7cc76f5aaf
Dean Corn <cornisath@gmail.com>
2018-03-14 20:53:14 -0400

    ng generates

--- Exercise/src/app/app.module.ts
+++ Exercise/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LogComponent } from './log/log.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
