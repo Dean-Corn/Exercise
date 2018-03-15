8c5d35d84f4947876b2783f49c4792757d469fd3
Dean Corn <cornisath@gmail.com>
2018-03-14 21:04:45 -0400

    more generate

--- Exercise/src/app/app.module.ts
+++ Exercise/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LogComponent } from './log/log.component';
 import { ProfileComponent } from './profile/profile.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
@@ -13,7 +15,8 @@ import { ProfileComponent } from './profile/profile.component';
     AppComponent,
     HomeComponent,
     LogComponent,
-    ProfileComponent
+    ProfileComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
